fecbe7cd8afbcafdf10c46e0c2a89e3c9ea9109f
Daniel Goman <gomanGGGoman@gmail.com>
2025-08-31 00:47:34 +0300

    Removed async from the bot service main

--- src/run_bot_service.py
+++ src/run_bot_service.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 
 
 from src.bot_service.telegram_bot import get_telegram_bot
@@ -9,14 +8,15 @@ from src.bot_service.telegram_bot import get_telegram_bot
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 
 
-async def main():
+def main():
     # Create the bot app
     bot_app = get_telegram_bot(TELEGRAM_BOT_TOKEN)
 
+    print("Starting Telegram Bot")
     # Run the bot (blocking)
-    await bot_app.run_polling()
+    bot_app.run_polling()
 
 
 # ---------------- Run ----------------
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
