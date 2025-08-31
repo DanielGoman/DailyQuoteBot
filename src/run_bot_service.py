505b3979c9c391902ebef31ebf014308b31427f0
Daniel Goman <gomanGGGoman@gmail.com>
2025-08-31 21:54:03 +0300

    Added usage of webhook instead of polling

--- src/run_bot_service.py
+++ src/run_bot_service.py
@@ -5,9 +5,12 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from src.bot_service.telegram_bot import get_telegram_bot
 
-
 # Environment variables
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+PORT = int(os.environ['PORT'])
+WEBHOOK_URL = os.environ['WEBHOOK_URL']
+WEBHOOK_PATH = f"/{TELEGRAM_BOT_TOKEN}"
+
 
 
 def main():
@@ -15,13 +18,21 @@ def main():
     bot_app = get_telegram_bot(TELEGRAM_BOT_TOKEN)
 
     print("Starting Telegram Bot")
-    # Run the bot (blocking)
-    bot_app.run_polling()
+    # TODO: replace Polling with Webhook, this polling is blocking AF and is causing problems on render
+
+    # Start webhook server
+    print(f"Starting webhook on port {PORT} at path {WEBHOOK_PATH}")
+    bot_app.run_webhook(
+        listen="localhost",
+        port=PORT,
+        url_path=WEBHOOK_PATH,
+        webhook_url=f"{WEBHOOK_URL}{WEBHOOK_PATH}"
+    )
 
 
 # --- Minimal dummy server ---
 def run_server():
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6000))
     server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
     print(f"Listening on port {port}")
     server.serve_forever()
